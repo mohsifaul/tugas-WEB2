abb698af13e57fc303f89d7008211a43532e2438
mohsifaul <azncm12345@gmail.com>
2022-09-10 09:49:27 +0700

    tambah komen

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
     $koneksi = mysqli_connect("localhost", "root", "", "wilayah");
-    if (!$koneksi) {
+    if (!$koneksi) { // jika gagal
         die("Connection failed: " . mysqli_connect_error());
     }
 ?>
